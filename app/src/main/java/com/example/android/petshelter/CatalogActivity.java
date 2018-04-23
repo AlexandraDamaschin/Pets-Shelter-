46ef22535b720d3d4134d431fcfa8d6c7c61b46b
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-23 20:19:07 +0100

    insertPet

--- app/src/main/java/com/example/android/petshelter/CatalogActivity.java
+++ app/src/main/java/com/example/android/petshelter/CatalogActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.petshelter;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -15,6 +16,8 @@ import com.example.android.petshelter.data.PetContract;
 import com.example.android.petshelter.data.PetDbHelper;
 
 public class CatalogActivity extends AppCompatActivity {
+    //database helper to provide access to the database
+    private PetDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +33,12 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //access database
+        mDbHelper = new PetDbHelper(this);
 
         displayDatabaseInfo();
         //to see database created
-       // PetDbHelper mDbHelper = new PetDbHelper(this);
+        // PetDbHelper mDbHelper = new PetDbHelper(this);
         //SQLiteDatabase db = mDbHelper.getReadableDatabase();
     }
 
@@ -44,7 +49,7 @@ public class CatalogActivity extends AppCompatActivity {
     private void displayDatabaseInfo() {
         // To access our database, we instantiate our subclass of SQLiteOpenHelper
         // and pass the context, which is the current activity.
-        PetDbHelper mDbHelper = new PetDbHelper(this);
+        //PetDbHelper mDbHelper = new PetDbHelper(this);
 
         // Create and/or open a database to read from it
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
@@ -78,7 +83,8 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
-                // Do nothing for now
+                insertPet();
+                displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
@@ -86,5 +92,27 @@ public class CatalogActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //methods needed
+    private void insertPet() {
+        //get db in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        //create content values
+        ContentValues values = new ContentValues();
+        values.put(PetContract.PetEntry.COLUMN_PET_NAME, "Toto");
+        values.put(PetContract.PetEntry.COLUMN_PET_BREED, "Terrier");
+        values.put(PetContract.PetEntry.COLUMN_PET_GENDER, PetContract.PetEntry.GENDER_MALE);
+        values.put(PetContract.PetEntry.COLUMN_PET_WEIGHT, 7);
+
+        //insert a new row in the database
+        // The first argument for db.insert() is the pets table name.
+        // The second argument provides the name of a column in which the framework
+        // can insert NULL in the event that the ContentValues is empty (if
+        // this is set to "null", then the framework will not insert a row when
+        // there are no values).
+        // The third argument is the ContentValues object containing the info for Toto.
+        long newRowId = db.insert(PetContract.PetEntry.TABLE_NAME, null, values);
     }
 }
