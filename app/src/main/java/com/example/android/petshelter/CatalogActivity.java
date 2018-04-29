d824852f6b13a42700e164278c7e350c4c3c73bd
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-29 15:20:30 +0100

    create pet cursor adapter to display list of pets in a listView

--- app/src/main/java/com/example/android/petshelter/CatalogActivity.java
+++ app/src/main/java/com/example/android/petshelter/CatalogActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.example.android.petshelter.data.PetContract;
 
@@ -84,48 +84,52 @@ public class CatalogActivity extends AppCompatActivity {
         */
         Cursor cursor = getContentResolver().query(PetContract.PetEntry.CONTENT_URI, projection, null, null, null);
 
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.list);
 
-        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+//        try {
+        // Create a header in the Text View that looks like this:
+        // _id - name - breed - gender - weight
+//            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
+//            displayView.append(PetContract.PetEntry._ID + " - " +
+//                    PetContract.PetEntry.COLUMN_PET_NAME + " - " +
+//                    PetContract.PetEntry.COLUMN_PET_BREED + " - " +
+//                    PetContract.PetEntry.COLUMN_PET_GENDER + " - " +
+//                    PetContract.PetEntry.COLUMN_PET_WEIGHT + "\n");
 
-        try {
-            // Create a header in the Text View that looks like this:
-            // _id - name - breed - gender - weight
-            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-            displayView.append(PetContract.PetEntry._ID + " - " +
-                    PetContract.PetEntry.COLUMN_PET_NAME + " - " +
-                    PetContract.PetEntry.COLUMN_PET_BREED + " - " +
-                    PetContract.PetEntry.COLUMN_PET_GENDER + " - " +
-                    PetContract.PetEntry.COLUMN_PET_WEIGHT + "\n");
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        PetCursorAdapter adapter = new PetCursorAdapter(this, cursor);
+        // Figure out the index of each column
+//            int idColumnIndex = cursor.getColumnIndex(PetContract.PetEntry._ID);
+//            int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+//            int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+//            int genderColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_GENDER);
+//            int weightColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_WEIGHT);
 
-            // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(PetContract.PetEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
-            int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
-            int genderColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_GENDER);
-            int weightColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_WEIGHT);
-
-            // Iterate through all the returned rows in the cursor
-            while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                String currentBreed = cursor.getString(breedColumnIndex);
-                int currentGender = cursor.getInt(genderColumnIndex);
-                int currentWeight = cursor.getInt(weightColumnIndex);
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" +
-                        currentID + " - " +
-                        currentName + " - " +
-                        currentBreed + " - " +
-                        currentGender + " - " +
-                        currentWeight));
-            }
-        } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
-        }
+        // Iterate through all the returned rows in the cursor
+//            while (cursor.moveToNext()) {
+//                // Use that index to extract the String or Int value of the word
+//                // at the current row the cursor is on.
+//                int currentID = cursor.getInt(idColumnIndex);
+//                String currentName = cursor.getString(nameColumnIndex);
+//                String currentBreed = cursor.getString(breedColumnIndex);
+//                int currentGender = cursor.getInt(genderColumnIndex);
+//                int currentWeight = cursor.getInt(weightColumnIndex);
+//                // Display the values from each column of the current row in the cursor in the TextView
+//                displayView.append(("\n" +
+//                        currentID + " - " +
+//                        currentName + " - " +
+//                        currentBreed + " - " +
+//                        currentGender + " - " +
+////                        currentWeight));
+//            }
+//        } finally {
+//            // Always close the cursor when you're done reading from it. This releases all its
+//            // resources and makes it invalid.
+//            cursor.close();
+//        }
+        // Attach the adapter to the ListView.
+        petListView.setAdapter(adapter);
     }
 
     @Override
@@ -154,6 +158,7 @@ public class CatalogActivity extends AppCompatActivity {
     }
 
     //methods needed
+
     private void insertPet() {
         //get db in write mode
         //SQLiteDatabase db = mDbHelper.getWritableDatabase();
