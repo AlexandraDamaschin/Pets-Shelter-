561665832f377f0089f6bc5e57949b3640c1cb4a
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-01 20:15:51 +0100

    Make a method for creating a “Discard changes” dialog

--- app/src/main/java/com/example/android/petshelter/EditorActivity.java
+++ app/src/main/java/com/example/android/petshelter/EditorActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.petshelter;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
@@ -299,5 +301,29 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
                         Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    // Show a dialog that warns the user there are unsaved changes that will be lost
+    //if they continue leaving the editor.
+    private void showUnsavedChangesDialog(
+            DialogInterface.OnClickListener discardButtonClickListener) {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the positive and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unsaved_changes_dialog_msg);
+        builder.setPositiveButton(R.string.discard, discardButtonClickListener);
+        builder.setNegativeButton(R.string.keep_editing, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Keep editing" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
